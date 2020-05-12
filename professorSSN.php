0daa94901c0038492ce8ad67faccc229575489b9
cThomp <cody5887@yahoo.com>
2020-05-11 17:54:25 -0700

    Update professorSSN.php

--- professorSSN.php
+++ professorSSN.php
@@ -22,11 +22,11 @@ $result = $conn->query($sql);
 if($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
         echo $row["TITLE"];
-        echo "     ";
+        echo "\t";
         echo $row["CNUM"];
-        echo "     ";
+        echo "\t";
         echo $row["DAYS"];
-        echo "     ";
+        echo "\t";
         echo $row["START"] . ' - ' . $row["END"];
         echo "<br>";
     }
