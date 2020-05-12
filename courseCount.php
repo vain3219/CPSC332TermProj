de367837c6fa3d7227edb15ede697fa5937c8621
twrexx <60373609+twrexx@users.noreply.github.com>
2020-05-11 18:13:09 -0700

    Add files via upload

---
+++ courseCount.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php 
+$CNUM = $_POST["cnum"];
+$SNUM = $_POST["snum"];
+$hostName = "localhost";
+$userName = "cs332t32";
+$password = "ooC6dein";
+$dbName = $userName;
+
+$conn = new mysqli($hostName, $userName, $password, $dbName);
+
+if($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "select e.GRADE, COUNT(GRADE) as 'NumberOf'
+        from enroll e
+        where e.cnum = $CNUM and e.sec_id = $SNUM
+        group by GRADE";
+$result = $conn->query($sql);
+
+if($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        echo $row["GRADE"];
+        echo "\t";
+        echo $row["COUNT"];
+        echo "<br>";
+    }
+}
+else {
+    echo "0 results";
+}
+
+$conn->close(); 
+?>
+</body>
+</html>
