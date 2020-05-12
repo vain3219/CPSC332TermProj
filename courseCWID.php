58d9cbddd5d7c716feedf0f22760b7d4f1e20e7a
twrexx <60373609+twrexx@users.noreply.github.com>
2020-05-11 18:39:35 -0700

    Add files via upload

---
+++ courseCWID.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php 
+$CWID = $_POST["CWID"];
+$hostName = "localhost";
+$userName = "cs332t32";
+$password = "ooC6dein";
+$dbName = $userName;
+
+$conn = new mysqli($hostName, $userName, $password, $dbName);
+
+if($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "select CNUM, GRADE
+        from ENROLL
+        where CWID = $CWID";
+$result = $conn->query($sql);
+
+if($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        echo $row["CNUM"];
+        echo "\t";
+        echo $row["GRADE"];
+        echo "<br>";
+    }
+}
+else {
+    echo "0 results";
+}
+
+$conn->close(); 
+?>
+</body>
+</html>
