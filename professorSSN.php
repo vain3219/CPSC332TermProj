2091ad910bfae7de538046903b3a368a7befdc1a
cThomp <cody5887@yahoo.com>
2020-05-11 17:25:06 -0700

    Update professorSSN.php

--- professorSSN.php
+++ professorSSN.php
@@ -15,9 +15,7 @@ if($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT C.COURSE_TITLE, S.CLASSROOM, S.MEETING_DAYS, S.BEGINNING_TIME, S.ENDING_TIME
-        FROM PROFESSOR P, COURSE C, COURSE_SECTIONS S
-        WHERE S.SSN_NUMBER = $SSN AND S.COURSE_ID = C.COURSE_ID";
+$sql = "select c.TITLE, s.CNUM, s.DAYS, s.START, s.END from PROF p, COURSE c, SECTION s where p.SSN = $SSN and p.SSN = s.INSTRUCTOR and s.CNUM = c.CNUM;";
 $result = $conn->query($sql);
 
 if($result->num_rows > 0) {
